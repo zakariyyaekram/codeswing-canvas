a9d8a3ea8165e6b4048a97729a22b2cbc012293e
zakariyyaekram <112276276+zakariyyaekram@users.noreply.github.com>
2022-08-28 14:58:10 -0500

    going to save

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ c.fillStyle = 'orange'
 c.font = 'small-caps bold 24px sans-serif'
 c.textAlign = 'center'
 c.fillText('Hello code hackerz!', 250, 200)
+
+var num = 30;
+var w = c.canvas.width;
+var h = c.canvas.height;
+
+for (var i = 0; i < num; i = i + 1) {
+  var x = i * w / (num - 1);
+  var y = i * h / (num - 1);
+  c.strokeStyle = randomRGBA();
+  drawLine(0, y, x, h);
+  c.strokeStyle = randomRGBA();
+  drawLine(x, 0, w, y);
+}
+
+function randomRGBA() {
+  var r = randInt(255);
+  var g = randInt(255);
+  var b = randInt(255);
+  var a = Math.random();
+  var rgba = [r,g,b,a].join(",");
+  return "rgba(" + rgba + ")";
+}
+function randInt(limit) {
+  var x = Math.random() * limit;
+  return Math.floor(x);
+}
+function drawLine(x1, y1, x2, y2) {
+  c.beginPath();
+  c.moveTo(x1, y1);
+  c.lineTo(x2, y2);
+  c.stroke();
+}
